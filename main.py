1da55945806e404e8410473cb85f02a33733518c
maxkgd89 <37918183+maxkgd89@users.noreply.github.com>
2025-04-15 13:06:05 +0000

    deleted to clean version

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from pygame.locals import QUIT
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
-pygame.draw.line(DISPLAYSURF,"white", (0,0),(400,400),5)
-pygame.draw.lines(DISPLAYSURF,"Yellow",False,[(50,100),(150,100),(150,150)],3)
-#small change 01
 
 while True:
    for event in pygame.event.get():
